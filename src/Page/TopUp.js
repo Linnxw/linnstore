cb36bf46bb2ed07816823a9c38a9fee46e7dedaa
Linnxw <ulin230606@gmail.com>
2023-03-18 19:26:33 +0700

    first commit

---
+++ src/Page/TopUp.js
@@ -0,0 +1,15 @@
+
+import Navbar from "../Component/Navbar"
+export default function Topup(){
+  return (
+    <>
+    <div className="absolute top-0">
+    <Navbar/>
+    </div>
+    <div className="w-screen h-screen bg-second flex items-center justify-center">
+
+      <h1 className="text-3xl text-slate-200 font-bold font-inter">NOT FOUND</h1>
+    </div>
+    </>
+    )
+}
